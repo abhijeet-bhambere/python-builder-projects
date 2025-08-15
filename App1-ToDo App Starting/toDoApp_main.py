94a7b88b543691bc9a758ef3e54fcdbe85014eb2
abhijeet-bhambere <abhijeetcbhambere@gmail.com>
2025-08-15 23:04:31 +0530

    accepts only upto 3 tasks ; added Main menu(add/view/exit)

--- App1-ToDo App Starting/toDoApp_main.py
+++ App1-ToDo App Starting/toDoApp_main.py
@@ -1,11 +1,31 @@
 # The ToDo App should follow the input>>processing>>output workflow
-# The approach of creating a variable for each todo is NOT sustainable for long-term
-# Hence we need to create function that will alter behaviour based on the user input
-usrinput = input("Enter in smallcase: ")
-result = usrinput.upper()
-print(result)
 
-# ================Test only==============
+# Create function that takes user input repeatedly (unless interrupted by user):
+# Hence, implementing a while loop with an incrementing variable ; also storing the ToDos in a list 
+todo_list = []
+
+n=0
+
+# Run for finite no. of loops -- accept only 3 tasks for now
+while n<3:
+    user_action = input("Type add / show/ exit: ")
+    
+    match user_action.lower().strip():
+        case 'add':
+            todo = input("Enter task: ")
+            todo_list.append(todo)
+            print(todo_list)
+            n+=1
+
+        case 'show':
+            print(f"Following tasks in the ToDo: {todo_list}")
+            for index, each in enumerate(todo_list):
+                print(f"{(index+1)} - {each}")
+
+        case 'exit':
+            break
+print("---------end of loop, bye!--------")
+# ================Day 1 Testing==============
 # user_input=input("Enter task: ")
 # print(f"User entered - {user_input}")
 # Use the list since we need a data type to collect the tasks
