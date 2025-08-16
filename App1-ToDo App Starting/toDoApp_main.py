c4036e13d8f1f284f483cfb9ba130663f6115576
abhijeet-bhambere <abhijeetcbhambere@gmail.com>
2025-08-16 23:04:46 +0530

    added control flow - wip

--- App1-ToDo App Starting/toDoApp_main.py
+++ App1-ToDo App Starting/toDoApp_main.py
@@ -1,37 +1,73 @@
 # The ToDo App should follow the input>>processing>>output workflow
-
-# Create function that takes user input repeatedly (unless interrupted by user):
-# Hence, implementing a while loop with an incrementing variable ; also storing the ToDos in a list 
+# Create such  that takes user input repeatedly (unless interrupted by user): also storing the ToDos in a list 
 todo_list = []
-
-n=0
-
-# Run for finite no. of loops -- accept only 3 tasks for now
-while n<3:
-    user_action = input("Type add / show/ exit: ")
-    
+edit_flag=False
+# Run until user opts to Exit:
+while True:
+    len_todo = len(todo_list)
+    user_action = input(">>>Type add / edit / show/ exit: ")
+    # using match-case for scenarios based on user input:
     match user_action.lower().strip():
+        # for case-1 : adding a task
         case 'add':
-            todo = input("Enter task: ")
+            todo = input(">>>Enter task: ")
             todo_list.append(todo)
-            print(todo_list)
-            n+=1
-
+            print(f"Task Added- {todo_list}\n")
+        # for case-2 : showing tasks added so far
         case 'show':
-            print(f"Following tasks in the ToDo: {todo_list}")
-            for index, each in enumerate(todo_list):
-                print(f"{(index+1)} - {each}")
+            if len_todo>0:
+                # print(f"Following tasks in the ToDo:")
+                # Intro message
+                if len_todo<=1:
+                    print(f"Your ToDo has {len_todo} open tasks:")
+                elif len_todo >1:
+                    print(f"Total {len_todo} tasks in ToDo:")
+                # Show the ToDo list
+                for index, each in enumerate(todo_list):
+                    print(f"{(index+1)} - {each}")
+                print("\n")
+            else: 
+                print("No tasks to show, add a few tasks first to edit\n")
+        # case-3: user opts to edit a task
+        case 'edit':
+            # Remain in edit mode until edit flag is reset
+            # Edit mode conditions 1 -- If ToDo list is empty, go back to main menu          
+            if len_todo==0:
+                print("No tasks to edit, add a few tasks first to edit\n")
+                continue
 
+            while True:
+                print(f">>>Select Task to edit (a no. between 1 - {len_todo}) OR Cancel :")
+                # show the ToDo list
+                for index, each in enumerate(todo_list):
+                    print(f"{(index+1)} - {each}")
+                edit_task_no = input(">>>Enter task no. OR type cancel:")
+                # Edit mode conditions--2
+                if((edit_task_no).lower()=="cancel"):
+                    print("Cancelling edit mode")
+                    break
+                
+                # Edit mode conditions--3
+                try: 
+                    edit_task_no == int(edit_task_no)
+                    
+                    if edit_task_no in range(1,len_todo+1):
+                        # check if entered task no. is in range of todo_list indexes
+                        # Actual index position in list would be n-1
+                        todo_list[(edit_task_no - 1)]=input(">>>Add updated task: ")
+                        print(f"Task no.{edit_task_no} updated !!\n")
+                        break
+                        # Task edit done! Exit the editing mode 
+                    else :
+                        edit_task_no = input(f">>>Enter a valid task no. between 1 - {len_todo}) OR Cancel :")
+                        if((edit_task_no).lower()=="cancel"):
+                            print("Cancelling edit mode")
+                            continue
+                except ValueError:
+                    print("\nInvalid input. Please enter a number.\n")
+    
+                
+            # for case-3: user opts to Exit
         case 'exit':
             break
-print("---------end of loop, bye!--------")
-# ================Day 1 Testing==============
-# user_input=input("Enter task: ")
-# print(f"User entered - {user_input}")
-# Use the list since we need a data type to collect the tasks
-# todo1=input("Enter todo1: ")
-# todo2=input("Enter todo2: ")
-# todo_list = [todo1, todo2]
-# print(todo_list)
-# The approach of creating a variable is NOT sustainable if we keep on adding tasks
-# Hence we need to create function that will alter behaviour based on the user input
+print("---------end of loop, bye!--------\n")
