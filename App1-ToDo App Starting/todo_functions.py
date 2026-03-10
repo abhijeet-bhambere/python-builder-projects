1d6cfc7df0d60c7a6815a12a78c2cb0cc3734385
abhijeet-bhambere <abhijeetcbhambere@gmail.com>
2026-03-10 23:00:16 +0530

    enhance get_todos function with file-handling for first-run scenario

--- App1-ToDo App Starting/todo_functions.py
+++ App1-ToDo App Starting/todo_functions.py
@@ -2,22 +2,35 @@
 """Module for the toDoApp main py file.
 
 Contains the `get_todos` and `write_todos` funcs"""
+
 import os
+# get the py file path
 pyfile_path = os.path.dirname(os.path.abspath(__file__))
+# 
 txtfile_path = os.path.join(pyfile_path,"todoList.txt")
-FILEPATH = txtfile_path
+FILEPATH = txtfile_path # defining  a constant 'FILEPATH' 
+
+
 # get_todo func--reads txt & displays the ToDos
 def get_todos(txtfile_path=FILEPATH):
-
     """reads txt file & displays the ToDos as a list"""
-    with open(txtfile_path,"r") as f:
-        contents = f.readlines()
-        todo_list_int = [x.strip("\n") for x in contents]
-    return todo_list_int
+    # Added file-handling at first-run scenario
+    try:
+        with open(txtfile_path,"r") as f:
+            contents = f.readlines()
+            todo_list_int = [x.strip("\n") for x in contents]
+        return todo_list_int
+    except FileNotFoundError:
+        with open(txtfile_path,"w") as f:
+            pass
+        print("📝 No existing list found — starting fresh!")
+        return []
+    
 
 
-# write_todo function is more like a procedure - not meant to return any value
-def write_todos(todo_list,txtfile_path=FILEPATH,):
+# write_todo function is more like a procedure
+# not meant to return any value ; rather update the txt file
+def write_todos(todo_list,txtfile_path=FILEPATH):
     """Writes the updated `todo_list` back into the txt file, its more like a procedure"""
     with open(txtfile_path,"w") as f:
         f.writelines([x + "\n" for x in todo_list])
